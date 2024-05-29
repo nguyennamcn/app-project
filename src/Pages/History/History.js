179dc448a862af460e50040bf05d69d5f32d5662
Thawn9703 <Thangndse171886@fpt.edu.vn>
2024-05-29 13:15:03 +0700

    Buy

---
+++ src/Pages/History/History.js
@@ -0,0 +1,12 @@
+// History.js
+import React from 'react';
+
+const History = () => {
+    return (
+        <div>
+            <p>This is the History component</p>
+        </div>
+    );
+};
+
+export default History;
