b414abe47d3b0f5b24627aa590014a39521c6ffc
Thawn9703 <Thangndse171886@fpt.edu.vn>
2024-05-29 21:23:43 +0700

    Modify BuyDiamond,BuyGold,BuyStoreProduct

--- src/Pages/BuyDiamond/BuyDiamond.js
+++ src/Pages/BuyDiamond/BuyDiamond.js
@@ -16,9 +16,9 @@ const BuyDiamond = () => {
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
-            padding: '20px',
+            padding: '30px',
             margin: 'auto',
-            backgroundColor: '#FFFFFF',
+            backgroundColor: '#A2F4EF',
             borderRadius: '8px',
             boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
             width: '80%',
@@ -37,7 +37,7 @@ const BuyDiamond = () => {
         inputRow: {
             display: 'flex',
             justifyContent: 'space-between',
-            marginBottom: '20px',
+            marginBottom: '10px',
             width: '100%',
         },
         label: {
@@ -45,6 +45,9 @@ const BuyDiamond = () => {
             flexDirection: 'column',
             flex: '1 1 auto',
             marginRight: '10px',
+            // padding: '5px 10px',
+            maxWidth: '300px',
+            maxHeight: '60%',
         },
         input: {
             padding: '10px',
@@ -53,7 +56,7 @@ const BuyDiamond = () => {
             backgroundColor:'#C2C2C2',
         },
         button: {
-            marginTop: '20px',
+            marginTop: '5px',
             backgroundColor: 'black',
             color: 'white',
             border: 'none',
@@ -77,7 +80,7 @@ const BuyDiamond = () => {
     return (
         <div>
             <div style={styles.container}>
-                <h1 style={styles.header}>Buy Gold #20001</h1>
+                <h1 style={styles.header}>Buy Diamond #20001</h1>
                 <form style={styles.form}>
                     <div style={styles.inputRow}>
                         <label style={styles.label}>
@@ -93,10 +96,16 @@ const BuyDiamond = () => {
                     <label style={styles.label}>
                                         Color:
                                         <select style={styles.input}value={color} onChange={e => setColor(e.target.value)}>
-                                            <option value="D">D</option>
-                                            <option value="E">E</option>
-                                            <option value="F">F</option>
-                                            {/* Add more options as needed */}
+                                        <option value=''>D</option>
+                                                <option value='E'>E</option>
+                                                <option value='F'>F</option>
+                                                <option value='G'>G</option>
+                                                <option value='H'>H</option>
+                                                <option value='I'>I</option>
+                                                <option value='J'>J</option>
+                                                <option value='K'>K</option>
+                                                <option value='L'>L</option>
+                                                <option value='M'>M</option>
                                         </select>
                                     </label>
 
@@ -111,22 +120,24 @@ const BuyDiamond = () => {
 
                     </div>                                      
                     <div style={styles.inputRow}>
-                    <label style={styles.label}>
+                                    <label style={styles.label}>
                                         Clarity:
                                         <select  style={styles.input} value={clarity} onChange={e => setClarity(e.target.value)}>
-                                            <option value="Internally Flawless (IF)">Internally Flawless (IF)</option>
-                                            <option value="Very Slightly Included (VS1)">Very Slightly Included (VS1)</option>
-                                            {/* Add more options as needed */}
+                                        <option value=''>IF</option>
+                                                <option value='VVS1'>VVS1</option>
+                                                <option value='VVS2'>VVS2</option>
+                                                <option value='VS1'>VS1</option>
+                                                <option value='VS2'>VS2</option>
+                                                <option value='SI1'>SI1</option>
+                                                <option value='SI2'>SI2</option>
+                                                <option value='I1'>I1</option>
+                                                <option value='I2'>I2</option>
                                         </select>
                                     </label>
 
                                     <label style={styles.label}>
-                                        Carat weight:
-                                        <select style={styles.input} value={caratWeight} onChange={e => setCaratWeight(parseFloat(e.target.value))}>
-                                            <option value={1.25}>1.25 ct (6.8 mm)</option>
-                                            <option value={0.75}>0.75 ct (5.9 mm)</option>
-                                            {/* Add more options as needed */}
-                                        </select>
+                                                Carat Weight:
+                                                <input style={styles.input} type="text" value={caratWeight} onChange={e => setCaratWeight(e.target.value)} />
                                     </label>
 
                             
