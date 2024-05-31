ef62f34da0adb649ca723115dbbb288c919c6433
TrunggHiieu281103 <thanhcong15426378@gmail.com>
2024-05-31 14:31:42 +0700

    update link API

--- src/Pages/DiamondPage/DiamondPage.js
+++ src/Pages/DiamondPage/DiamondPage.js
@@ -1,8 +1,9 @@
 
 import React from 'react'
 import { useState, useEffect } from 'react'
-import { productServ } from './productService'
+import { productServ } from './diamondService'
 import { Card, List } from 'antd';
+import { NavLink } from 'react-router-dom';
 import './DiamondPage.css'
 const { Meta } = Card;
 
@@ -15,32 +16,31 @@ export default function DiamondPage() {
     img: 'https://trangsuc.doji.vn/Upload/dong-bo/kim-cuong-vien/kim-cuong-vien.jpg',
   }));
 
-  const [products, setItem] = useState([]);
+  const [products, setProducts] = useState([]);
+
   useEffect(() => {
     productServ.getProductList()
       .then((res) => {
-        console.log(res);
-        setItem(res.data.content)
+        console.log(res.data.metadata.data);
+        setProducts(res.data.metadata.data);
       })
       .catch((err) => {
         console.log(err);
-      }
-      )
-  }, [])
-
-  console.log("result", products);
+      });
+  }, []);
   return (
+
+  <div className="gold-container">
+  <div className='filter' style={{
+    display: 'flex',
+    marginTop: '-6px',
+    marginBottom: '6px',
+    marginLeft: '22px',
+    justifyContent: 'space-between',
+  }}>
+
     <div>
-
-    <div className='filter' style={{
-      display: 'flex',
-      marginBottom: '10px',
-      marginLeft:'22px',
-      justifyContent: 'space-between',
-    }}>
-
-      <div>
-        <select name='jewelry__category' id='Carat'>
+    <select name='jewelry__category' id='Carat'>
           <option value='Carat'>Carat</option>
           <option value='Carat'>0.30-0.49</option>
           <option value='Carat'>0.50-0.89</option>
@@ -84,113 +84,82 @@ export default function DiamondPage() {
           <option value=''>I1</option>
           <option value=''>I2</option>
         </select>
-
-        <select name='jewelry__price' id='price'>
-          <option value=''>0-5000$</option>
-          <option value=''>5000$-10000$</option>
-          <option value=''>10.000$-20.000$</option>
-          <option value=''>20.000$-50.000$</option>
-          <option value=''>50.000$-100.000$</option>
-          <option value=''>over 100.000$</option>
-        </select>
-      </div>
-
-      <div className='search__input ' style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: '6px',
-        border: 'solid black 1px',
-        borderRadius: '20px',
-      }}>
-        <input type='text' placeholder='Search...' style={{ outline: 'none', margin: '-4px 0 -4px 8px' }}></input>
-        <svg style={{ marginRight: '10px' }} xmlns="http://www.w3.org/2000/svg" maxWidth="18" height="18" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-        </svg>
-      </div>
-
     </div>
-    <div className='contain__list'
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-      }}
-    >
-      <List
-        style={{
-          maxWidth: '95%',
-        }}
-        grid={{ gutter: 11, column: 5 }}
-        size='small'
-        itemLayout='vertical'
-        pagination={{
-          onChange: (page) => {
-            console.log(page);
-          },
-          pageSize: 10,
-          position: 'bottom',
-          align: 'end',
-          className: 'custom__pagination',
-        }}
-        dataSource={data} //lấy data set sẵn để test
-        renderItem={(item) => (
-          <List.Item
-            className='list__products'
-            style={{
-              padding: '0 0',
-            }}
-          >
-            <Card
-              className='card'
-              hoverable
-              bodyStyle={{ padding: '8px' }}
-              style={{
-                maxWidth: '240px',
-                textAlign: 'center',
-                borderRadius: '10px',
-              }}
-              cover={
-                <img
-                  style={{ maxWidth: '100%', height: '120px', objectFit: 'cover' }}
-                  alt="example"
-                  src={item.img}
-                />
-              }
-            >
-              <Meta
-                style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
-                  padding: '0',
-                }}
-                title={
-                  <span style={{
-                    fontSize: '11.4px',
-                    whiteSpace: 'normal',
-                    marginBottom: '0',
-                    display: 'block',
-                  }}>
-                    {item.title} {item.code}
-                  </span>
-                }
-                description={
-                  <span style={{
-                    textAlign: 'center',
-                    marginTop: '4px',
-                  }}>
-                    {item.cost}
-                  </span>
-                }
-              />
-            </Card>
 
-          </List.Item>
-        )}
-      />
+    <div className='search__input ' style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: '6px',
+      border: 'solid black 1px',
+      borderRadius: '20px',
+    }}>
+      <input type='text' placeholder='Search...' style={{ outline: 'none', margin: '-4px 0 -4px 8px' }}></input>
+      <svg style={{ marginRight: '10px' }} xmlns="http://www.w3.org/2000/svg" maxWidth="18" height="18" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+      </svg>
     </div>
 
   </div>
+  <List
+    style={{
+      maxWidth: '100%',
+      marginLeft: '30px',
+      height: '100px',
+    }}
+    grid={{ gutter: 11, column: 5 }}
+    size='small'
+    itemLayout='vertical'
+    pagination={{
+      onChange: (page) => {
+        console.log(page);
+      },
+      pageSize: 10,
+      position: 'bottom',
+      align: 'end',
+      className: 'custom__pagination',
+    }}
+
+    dataSource={products} //lấy data set sẵn để test
+    renderItem={(sp) => (
+      <List.Item
+        className='list__products'
+        style={{
+          padding: '0 0',
+          height: '210px'
+        }}
+      >
+        <div
+          className="product-card-container"
+          key={sp.productCode}>
+
+          <NavLink style={{ textDecoration: 'none' }} to={`/detail/${sp.productCode}`}>
+            <Card
+              hoverable
+              bodyStyle={{ padding: '8px' }}
+              style={{
+                width: '220px',
+                textAlign: 'center',
+                borderRadius: '10px',
+              }}
+              cover={<img style={{ padding: '10px', maxWidth: '100%', height: '150px', objectFit: 'cover', }} alt={sp.productName} src={sp.productImage} />}
+            >
+              <Meta
+                title={<span style={{ fontSize: '14px' }}>{sp.productName}</span>}
+                description={sp.categoryType} />
+              <div className="product-info">
+                <h1>{sp.productCode}</h1>
+                <h2> {sp.productPrice}$</h2>
+              </div>
+            </Card>
+          </NavLink>
+
+        </div>
+      </List.Item>
+    )}
+  >
+  </List>
+</div>
   )
 }
 
--- src/Pages/GoldPage/GoldPage.js
+++ src/Pages/GoldPage/GoldPage.js
@@ -1,89 +1,64 @@
 
 import React from 'react'
 import { useState, useEffect } from 'react'
-import { productServ } from './productService'
+import { productServ } from './goldService'
 import { Card, List } from 'antd';
-import './GoldPage.css'
+import { NavLink } from 'react-router-dom';
+import './GoldPage.css';
 const { Meta } = Card;
 
 export default function GoldPage() {
 
-  const data = Array.from({ length: 51 }).map((_, i) => ({
-    title: `Title aaaaaaa aaaaaaaaaaaaa aaaaaaaaa aaaaa${i}`,
-    code: `Code ${i}`,
-    cost: `Cost ${i}`,
-    img: 'https://cdn.pnj.io/images/detailed/210/sp-gz0000y000980-vang-tai-loc-passport-0.3-chi-24k-pnj-1.png',
-  }));
+  const [products, setProducts] = useState([]);
 
-  const [products, setItem] = useState([]);
   useEffect(() => {
     productServ.getProductList()
       .then((res) => {
-        console.log(res);
-        setItem(res.data.content)
+        console.log(res.data.metadata.data);
+        setProducts(res.data.metadata.data);
       })
       .catch((err) => {
         console.log(err);
-      }
-      )
-  }, [])
+      });
+  }, []);
 
-  console.log("result", products);
   return (
-    <div>
-
-    <div className='filter' style={{
-      display: 'flex',
-      marginBottom: '10px',
-      marginLeft:'22px',
-      justifyContent: 'space-between',
-    }}>
-
-      <div>
-        <select name='jewelry__category' id='category'>
-          <option value='gold-ring'>Gold ring</option>
-          <option value='gold-bars'>Gold bars</option>
-        </select>
-
-        {/* <select name='jewelry__gender' id='gender'>
-          <option value='male'>Male</option>
-          <option value='female'>Female</option>
-        </select> */}
-
-        {/* <select name='jewelry__price' id='price'>
-          <option value=''>0-5000$</option>
-          <option value=''>5000$-10000$</option>
-          <option value=''>10.000$-20.000$</option>
-          <option value=''>20.000$-50.000$</option>
-          <option value=''>50.000$-100.000$</option>
-          <option value=''>over 100.000$</option>
-        </select> */}
-      </div>
-
-      <div className='search__input ' style={{
+    <div className="gold-container">
+      <div className='filter' style={{
         display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: '6px',
-        border: 'solid black 1px',
-        borderRadius: '20px',
+        marginTop: '-6px',
+        marginBottom: '6px',
+        marginLeft: '22px',
+        justifyContent: 'space-between',
       }}>
-        <input type='text' placeholder='Search...' style={{ outline: 'none', margin: '-4px 0 -4px 8px' }}></input>
-        <svg style={{ marginRight: '10px' }} xmlns="http://www.w3.org/2000/svg" maxWidth="18" height="18" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-        </svg>
-      </div>
 
-    </div>
-    <div className='contain__list'
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-      }}
-    >
+        <div>
+          <select name='jewelry__category' id='category'>
+            <option value='gold-ring'>Gold ring</option>
+            <option value='gold-bars'>Gold bars</option>
+          </select>
+        </div>
+
+        <div className='search__input ' style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: '6px',
+          border: 'solid black 1px',
+          borderRadius: '20px',
+        }}>
+          <input type='text' placeholder='Search...' style={{ outline: 'none', margin: '-4px 0 -4px 8px' }}></input>
+          <svg style={{ marginRight: '10px' }} xmlns="http://www.w3.org/2000/svg" maxWidth="18" height="18" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+          </svg>
+        </div>
+
+      </div>
       <List
         style={{
-          maxWidth: '95%',
+          maxWidth: '100%',
+          marginLeft: '30px',
+          height: '100px',
         }}
         grid={{ gutter: 11, column: 5 }}
         size='small'
@@ -97,66 +72,48 @@ export default function GoldPage() {
           align: 'end',
           className: 'custom__pagination',
         }}
-        dataSource={data} //lấy data set sẵn để test
-        renderItem={(item) => (
+
+        dataSource={products} //lấy data set sẵn để test
+        renderItem={(sp) => (
           <List.Item
             className='list__products'
             style={{
               padding: '0 0',
+              height: '210px'
             }}
           >
-            <Card
-              className='card'
-              hoverable
-              bodyStyle={{ padding: '8px' }}
-              style={{
-                maxWidth: '240px',
-                textAlign: 'center',
-                borderRadius: '10px',
-              }}
-              cover={
-                <img
-                  style={{ maxWidth: '100%', height: '120px', objectFit: 'cover' }}
-                  alt="example"
-                  src={item.img}
-                />
-              }
-            >
-              <Meta
-                style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
-                  padding: '0',
-                }}
-                title={
-                  <span style={{
-                    fontSize: '11.4px',
-                    whiteSpace: 'normal',
-                    marginBottom: '0',
-                    display: 'block',
-                  }}>
-                    {item.title} {item.code}
-                  </span>
-                }
-                description={
-                  <span style={{
-                    textAlign: 'center',
-                    marginTop: '4px',
-                  }}>
-                    {item.cost}
-                  </span>
-                }
-              />
-            </Card>
+            <div
+              className="product-card-container"
+              key={sp.productCode}>
 
+              <NavLink style={{ textDecoration: 'none' }} to={`/detail/${sp.productCode}`}>
+                <Card
+                  hoverable
+                  bodyStyle={{ padding: '8px' }}
+                  style={{
+                    width: '220px',
+                    textAlign: 'center',
+                    borderRadius: '10px',
+                  }}
+                  cover={<img style={{ padding: '10px', maxWidth: '100%', height: '150px', objectFit: 'cover', }} alt={sp.productName} src={sp.productImage} />}
+                >
+                  <Meta
+                    title={<span style={{ fontSize: '14px' }}>{sp.productName}</span>}
+                    description={sp.categoryType} />
+                  <div className="product-info">
+                    <h1>{sp.productCode}</h1>
+                    <h2> {sp.productPrice}$</h2>
+                  </div>
+                </Card>
+              </NavLink>
+
+            </div>
           </List.Item>
         )}
-      />
+      >
+      </List>
     </div>
-
-  </div>
-  )
+  );
 }
 
 
