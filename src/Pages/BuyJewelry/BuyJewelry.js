4a48816b7ffeab330cc7a2035fe044f126f96d5e
Thawn9703 <Thangndse171886@fpt.edu.vn>
2024-06-09 10:52:43 +0700

    Update Buy Jewelry

--- src/Pages/BuyJewelry/BuyJewelry.js
+++ src/Pages/BuyJewelry/BuyJewelry.js
@@ -6,7 +6,7 @@ import { useSelector } from 'react-redux';
 const styles = {
   container: {
     background: '#AFBEF9',
-    padding: '10px',
+    padding: '15px',
     maxWidth: '900px',
     margin: '20px auto',
     borderRadius: '10px',
@@ -29,7 +29,7 @@ const styles = {
     fontWeight: 'bold',
   },
   input: {
-    padding: '0px',
+    padding: '3px',
     border: '2px solid #cccccc',
     borderRadius: '5px',
     fontSize: '16px'
@@ -38,7 +38,7 @@ const styles = {
     backgroundColor: '#222222',
     color: 'white',
     border: 'none',
-    padding: '0px 20px',
+    padding: '5px 15px',
     borderRadius: '5px',
     cursor: 'pointer',
     fontSize: '18px',
@@ -63,21 +63,22 @@ const GoldSelection = () => {
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
   const [weight, setWeight] = useState('');
-  const [productId, setProductId] = useState('None');
+  const [goldType, setGoldType] = useState('');
+  const [diamond, setDiamond] = useState('');
+  const [color, setColor] = useState('');
+  const [cut, setCut] = useState('');
+  const [clarity, setClarity] = useState('');
+  const [caratWeight, setCaratWeight] = useState('');
   const [totalPrice, setTotalPrice] = useState(340000000);
 
-
-  let userInfo = useSelector((state) => {
-    return state.userReducer.userInfo;
-  })
-  console.log(userInfo);
+  let userInfo = useSelector((state) => state.userReducer.userInfo);
 
   useEffect(() => {
     if (customerData) {
       setName(customerData.customerName);
       setPhone(customerData.phone);
       // Set other fields as needed from customerData
-      console.log(customerData)
+      console.log(customerData);
     }
   }, [customerData]);
 
@@ -89,8 +90,7 @@ const GoldSelection = () => {
       phone: phone,
       list: [
         {
-          name: 'Jewelry' ,
-          productId: productId,
+          name: 'Jewelry',
           weight: parseFloat(weight),
           origin: 'Unknown',
           price: totalPrice
@@ -131,15 +131,104 @@ const GoldSelection = () => {
           <input type="text" style={styles.input} value={phone} onChange={e => setPhone(e.target.value)} />
         </div>
         <div style={styles.formGroup}>
-          <label style={styles.label}>ProductId:</label>
-          <input type="text" style={styles.input} value={productId} onChange={e => setProductId(e.target.value)} />
+          <label style={styles.label}>Gold type:</label>
+          <select style={styles.input} value={goldType} onChange={e => setGoldType(e.target.value)}>
+            <option value=""></option>
+            <option value="None">None</option>
+            <option value="Gold Bars">Gold Bars</option>
+            <option value="24K Gold (990)">24K Gold (990)</option>
+            <option value="18K Gold (750)">18K Gold (750)</option>
+            <option value="White Gold Au750">White Gold Au750</option>
+          </select>
         </div>
         <div style={styles.formGroup}>
           <label style={styles.label}>Weight (gram):</label>
-          <input type="number" style={styles.input} value={weight} onChange={e => setWeight(e.target.value)} />
+          <input
+            type="number"
+            style={styles.input}
+            value={weight}
+            onChange={e => setWeight(e.target.value)}
+            disabled={goldType === 'None'}
+          />
         </div>
-        
-        
+        <div style={styles.formGroup}>
+          <label style={styles.label}>Diamond:</label>
+          <select style={styles.input} value={diamond} onChange={e => setDiamond(e.target.value)}>
+            <option value=""></option>
+            <option value="None">None</option>
+            <option value="Natural">Natural</option>
+            <option value="Synthetic">Synthetic</option>
+          </select>
+        </div>
+        <div></div>
+        <div style={styles.formGroup}>
+          <label style={styles.label}>Carat Weight:</label>
+          <input
+            type="number"
+            style={styles.input}
+            value={caratWeight}
+            onChange={e => setCaratWeight(e.target.value)}
+            disabled={diamond === 'None'}
+          />
+        </div>
+        <div style={styles.formGroup}>
+          <label style={styles.label}>Color:</label>
+          <select
+            style={styles.input}
+            value={color}
+            onChange={e => setColor(e.target.value)}
+            disabled={diamond === 'None'}
+          >
+            <option value=''></option>
+            <option value='D'>D</option>
+            <option value='E'>E</option>
+            <option value='F'>F</option>
+            <option value='G'>G</option>
+            <option value='H'>H</option>
+            <option value='I'>I</option>
+            <option value='J'>J</option>
+            <option value='K'>K</option>
+            <option value='L'>L</option>
+            <option value='M'>M</option>
+          </select>
+        </div>
+        <div style={styles.formGroup}>
+          <label style={styles.label}>Clarity:</label>
+          <select
+            style={styles.input}
+            value={clarity}
+            onChange={e => setClarity(e.target.value)}
+            disabled={diamond === 'None'}
+          >
+            <option value=''></option>
+            <option value='FL'>FL</option>
+            <option value='IF'>IF</option>
+            <option value='VVS1'>VVS1</option>
+            <option value='VVS2'>VVS2</option>
+            <option value='VS1'>VS1</option>
+            <option value='VS2'>VS2</option>
+            <option value='SI1'>SI1</option>
+            <option value='SI2'>SI2</option>
+            <option value='I1'>I1</option>
+            <option value='I2'>I2</option>
+            <option value='I3'>I3</option>
+          </select>
+        </div>
+        <div style={styles.formGroup}>
+          <label style={styles.label}>Cut:</label>
+          <select
+            style={styles.input}
+            value={cut}
+            onChange={e => setCut(e.target.value)}
+            disabled={diamond === 'None'}
+          >
+            <option value=""></option>
+            <option value="EX">EX</option>
+            <option value="G">G</option>
+            <option value="F">F</option>
+            <option value="P">P</option>
+          </select>
+        </div>
         <div style={styles.totalPrice}>
           Total price: {totalPrice.toLocaleString()}
         </div>
